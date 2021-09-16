9531e9bf7ae5b9ea7679987d5b3f2d8547d678b8
elieser pereira <elieser.pereiraa@gmail.com>
2021-09-16 09:56:03 -0300

    set floor and remove requests

--- rust/liftkata/src/lift.rs
+++ rust/liftkata/src/lift.rs
@@ -42,6 +42,14 @@ impl System {
     pub fn add_calls(&mut self, calls: &mut Vec<Call>){
         self.calls.append(calls)
     }
+
+    pub fn set_lift_floor(&mut self, lift_index:i32, floor: i32){
+        self.lifts[lift_index as usize].floor = floor
+    }
+
+    pub fn remove_request(&mut self, lift_index:i32, index: i32){
+        self.lifts[lift_index as usize].requests.remove(index as usize);
+    }
 }
 
 #[cfg(test)]
@@ -71,7 +79,28 @@ mod tests {
     }
     #[test]
     pub fn test2() {
-        assert_eq!(1,2)
+        let direction = Direction::Up;
+        let call = Call {floor: 1, direction: direction};
+        let callb = Call {floor: 2, direction: direction};
+        let callc = Call {floor: 3, direction: direction};
+        println!("{:?}", direction);
+        println!("{:?}", call);
+        let lifta = Lift {id: "A".into(), floor: 1, requests: vec![], doors_open: true};
+        let liftb = Lift {id: "B".into(), floor: 1, requests: vec![], doors_open: true};
+        let liftc = Lift {id: "C".into(), floor: 1, requests: vec![], doors_open: true};
+        println!("{:?}", lifta);
+        let mut system = System::new(vec![], vec![lifta], vec![call]);
+        println!("{:?}", system);
+        system.add_floors(vec![1,2,3]);
+        let mut lifts = vec![liftb, liftc];
+        system.add_lifts(&mut lifts);
+        let mut calls = vec![callb, callc];
+        system.add_calls(&mut calls);
+        system.set_lift_floor(0, 2);
+        assert_eq!(system.floors,vec![1,2,3]);
+
+        assert_eq!(system.lifts[0].floor,2);
+        //system.remove_request(0, 2);// lift index  y index(piso)
     }
 }
 
